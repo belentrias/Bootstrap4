284cfa71cef19c6bd919cae1ae79e8a5e3d3a214
Belén Trías <belentrias27@gmail.com>
2021-01-20 16:48:20 -0300

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -135,11 +135,12 @@ module.exports = function(grunt){
             }
         }
     });
-    grunt.loadNpmTasks('grunt-contrib-watch');
+   /* grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-browser-sync');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin'); */
     grunt.registerTask('css', ['sass']);
+    grunt.registerTask('usemin', ['useminPrepare']);
     grunt.registerTask('default',['browserSync', 'watch']);
     grunt.registerTask('img:compress',['imagemin']);
     grunt.registerTask('build',[
